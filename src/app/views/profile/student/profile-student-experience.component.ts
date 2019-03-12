34e495889ccb203729e19946781b55a9565390fb
Mikel Martín <mikel.martin@edooca.com>
2019-03-12 20:29:21 +0100

    formulario de experiencia profesional terminado

--- src/app/views/profile/student/profile-student-experience.component.ts
+++ src/app/views/profile/student/profile-student-experience.component.ts
@@ -10,90 +10,29 @@ export class ProfileStudentExperienceComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   profileStudentExperienceForm = this.fb.group({
-    titletype: ["", Validators.required],
-    university: this.fb.group({
-      universitycentre: [""],
-      universitytitle: [""],
-      universitytitledate: [
-        "",
-        Validators.pattern("^\\d{1,2}/\\d{1,2}/\\d{4}$")
-      ],
-      universitybiling: [""],
-      universitycert: [""]
-    }),
-    cicle: this.fb.group({
-      ciclecentre: [""],
-      ciclefamily: [""],
-      ciclegrade: [""],
-      cicletitle: [""],
-      cicletitledate: ["", Validators.pattern("^\\d{1,2}/\\d{1,2}/\\d{4}$")],
-      cicledual: [""],
-      ciclebiling: [""],
-      ciclecert: [""]
-    }),
-    other: this.fb.group({
-      othertitle: [""]
-    })
+    companie: [
+      "",
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(55),
+        Validators.pattern("^[a-zA-Z0-9_-][a-zA-Z0-9_s-]+$")
+      ]
+    ],
+    startdate: ["", Validators.pattern("^\\d{1,2}/\\d{1,2}/\\d{4}$")],
+    enddate: ["", Validators.pattern("^\\d{1,2}/\\d{1,2}/\\d{4}$")],
+    job: [
+      "",
+      [
+        Validators.pattern("^[a-zA-Z0-9_-][a-zA-Z0-9_s-]+$"),
+        Validators.minLength(3),
+        Validators.maxLength(55)
+      ]
+    ],
+    tasks: [""]
   });
 
-  ngOnInit() {
-    this.profileStudentExperienceForm
-      .get("titletype")
-      .valueChanges.subscribe((titletype: string) => {
-        this.profileStudentExperienceForm
-          .get("university")
-          .get("universitytitle")
-          .setValidators(null);
-        this.profileStudentExperienceForm
-          .get("cicle")
-          .get("cicletitle")
-          .updateValueAndValidity();
-          this.profileStudentExperienceForm
-          .get("cicle")
-          .get("cicletitle")
-          .setValidators(null);
-        this.profileStudentExperienceForm
-          .get("other")
-          .get("othertitle")
-          .updateValueAndValidity();
-          this.profileStudentExperienceForm
-          .get("other")
-          .get("othertitle")
-          .setValidators(null);
-        this.profileStudentExperienceForm
-          .get("university")
-          .get("universitytitle")
-          .updateValueAndValidity();
-        if (titletype === "university") {
-          this.profileStudentExperienceForm
-            .get("university")
-            .get("universitytitle")
-            .setValidators([Validators.required]);
-          this.profileStudentExperienceForm
-            .get("university")
-            .get("universitytitle")
-            .updateValueAndValidity();
-        } else if (titletype === "cicle") {
-          this.profileStudentExperienceForm
-            .get("cicle")
-            .get("cicletitle")
-            .setValidators([Validators.required]);
-          this.profileStudentExperienceForm
-            .get("cicle")
-            .get("cicletitle")
-            .updateValueAndValidity();
-        } else if (titletype === "other") {
-          this.profileStudentExperienceForm
-            .get("other")
-            .get("othertitle")
-            .setValidators([Validators.required]);
-          this.profileStudentExperienceForm
-            .get("other")
-            .get("othertitle")
-            .updateValueAndValidity();
-        }
-      });
-  }
+  ngOnInit() {}
 
   get f() {
     return this.profileStudentExperienceForm.controls;
