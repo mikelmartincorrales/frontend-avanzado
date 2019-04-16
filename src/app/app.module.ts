81124b3cdd322315479972bc53820c28d61bdcc7
Mikel Martín <mikel.martin@edooca.com>
2019-04-16 20:33:28 +0200

    dev-tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './shared/core.module';
-import { RouterModule } from '@angular/router';
-import { rootRouterConfig } from './app-routing';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { FakeBackendService } from './shared/inmemory-db/fake-backend.service';
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./shared/core.module";
+import { RouterModule } from "@angular/router";
+import { rootRouterConfig } from "./app-routing";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { FakeBackendService } from "./shared/inmemory-db/fake-backend.service";
+import { StoreModule } from "@ngrx/store";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
+import { reducers, metaReducers } from "./reducers";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
 
 @NgModule({
   imports: [
@@ -16,7 +21,10 @@ import { FakeBackendService } from './shared/inmemory-db/fake-backend.service';
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(FakeBackendService, {
       dataEncapsulation: false
-    })
+    }),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot({ stateKey: "router" })
   ],
   declarations: [AppComponent],
   providers: [],
--- src/app/shared/state/root.reducer.ts
+++ src/app/shared/state/root.reducer.ts
@@ -1,1 +1,9 @@
 // root-reducer
+import { routerReducer } from "@ngrx/router-store";
+import { ActionReducerMap } from "@ngrx/store";
+
+export interface AppState {}
+
+export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer
+};
