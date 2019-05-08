87e6972400aa343762db0f113c25273207243ae8
Mikel Martín <mikel.martin@edooca.com>
2019-05-08 13:09:32 +0200

    mensajes de error

--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MaterialModule } from '../../shared/material.module';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [SharedModule, DashboardRoutingModule]
+  imports: [SharedModule, DashboardRoutingModule, MaterialModule]
 })
-export class DashboardModule {}
+export class DashboardModule { }
